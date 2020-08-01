a299dd590f5215608db137dd984091b43f3d7338
hiroomifujiwara <hiro.03.fujiwara@gmail.com>
2020-08-01 13:39:12 +0900

    上端点が1で下端点が1の閉区間オブジェクトは生成できるのテストメソッドを実装

---
+++ src/test/java/tddbc/ClosedRangeTest.java
@@ -0,0 +1,16 @@
+package tddbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ClosedRangeTest {
+
+    @Test
+    public void 上端点が1で下端点が1の閉区間オブジェクトは生成できる() {
+
+        ClosedRange actual;
+        assertNotNull(acutal);
+    }
+}
