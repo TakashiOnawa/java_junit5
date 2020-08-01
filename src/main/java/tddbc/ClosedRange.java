dd6d2acfe1199c47d43c85226b16f2cb7cd7f447
hiroomifujiwara <hiro.03.fujiwara@gmail.com>
2020-08-01 14:27:37 +0900

    整数閉区間オブジェクトは下端点と上端点を持つを実装

--- src/main/java/tddbc/ClosedRange.java
+++ src/main/java/tddbc/ClosedRange.java
@@ -1,9 +1,16 @@
 package tddbc;
 
 public class ClosedRange {
+
+    private final int lowerEndpoint;
+    private final int upperEndpoint;
+
     public ClosedRange(int lowerEndpoint, int upperEndpoint) {
-        if(lowerEndpoint > upperEndpoint) {
+        if (lowerEndpoint > upperEndpoint) {
             throw new IllegalArgumentException();
         }
+
+        this.lowerEndpoint = lowerEndpoint;
+        this.upperEndpoint = upperEndpoint;
     }
 }
